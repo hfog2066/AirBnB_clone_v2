96a66a5bc4df0691746fcd5268e1e54b9537ba86
hfog2066 <hfog2000@yahoo.com>
2020-12-22 20:12:38 -0500

    Esto es un matadero

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,11 +10,13 @@ def do_pack():
     """ Create tar """
     savedir = "versions/"
     filename = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+
     if not os.path.exists(savedir):
         os.mkdir(savedir)
-    with tarfile.open(savedir + filename, "w:gz") as tar:
-        tar.add("web_static", arcname=os.path.basename("web_static"))
-    if os.path.exists(savedir + filename):
-        return savedir + filename
-    else:
-        return None
+        with tarfile.open(savedir + filename, "w:gz") as tar:
+            tar.add("web_static", arcname=os.path.basename("web_static"))
+            if os.path.exists(savedir + filename):
+                return savedir + filename
+
+            else:
+                return None
