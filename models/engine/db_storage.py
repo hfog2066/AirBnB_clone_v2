d6beaf95de05d1b7cf6cdcae77c6b82a995c5e15
hfog2066 <hfog2000@yahoo.com>
2021-01-19 16:59:46 -0500

    engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,72 +1,86 @@
 #!/usr/bin/python3
-""" DBStorage class """
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine
-from models.base_model import Base
-from models.state import State
-from models.city import City
-from models.user import User
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-import os
+"""Module for DBstorage class"""
+from os import getenv
+from sqlalchemy import create_engine, MetaData
 
 
-class DBStorage:
-    """[DBStorage]"""
+class DBStorage():
+    """Class for database storage"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """__init__"""
-        user = os.getenv('HBNB_MYSQL_USER')
-        password = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST')
-        database = os.getenv('HBNB_MYSQL_DB')
-        environment = os.getenv('HBNB_ENV')
-
+        """Initializes storage"""
+        from models.base_model import Base
         self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'.format(
-                user, password, host, database), pool_pre_ping=True)
-
-        if environment == 'test':
-            Base.metadata.drop_all(self.__engine)
+            'mysql+mysqldb://{}:{}@{}:3306/{}'
+            .format(getenv("HBNB_MYSQL_USER"),
+                    getenv("HBNB_MYSQL_PWD"),
+                    getenv("HBNB_MYSQL_HOST"),
+                    getenv("HBNB_MYSQL_DB")),
+            pool_pre_ping=True)
 
     def all(self, cls=None):
-        """ Return all dbstorage """
-        queryClass = [City, State, User, Place, Review]
-        queryList_Output = []
-        queryDict_Output = {}
-        if cls in queryClass:
-            queryList_Output = self.__session.query(cls).all()
+        """returns all objects of cls"""
+        from models.state import State
+        from models.city import City
+        from models.user import User
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        class_list = [
+            State,
+            City,
+            User,
+            Place,
+            Review,
+            Amenity
+        ]
+        rows = []
+        if cls:
+            rows = self.__session.query(cls)
         else:
-            for class_Input in queryClass:
-                queryList_Output += self.__session.query(class_Input).all()
-        for class_Output in queryList_Output:
-            queryDict_Output.update(
-                {class_Output.__class__.__name__ + "." + class_Output.id:
-                    class_Output})
-        return queryDict_Output
+            for cls in class_list:
+                rows += self.__session.query(cls)
+        return {type(v).__name__ + "." + v.id: v for v in rows}
 
     def new(self, obj):
-        """ Create a new obj """
-        if obj:
-            self.__session.add(obj)
+        """add object to db"""
+        if not obj:
+            return
+        self.__session.add(obj)
 
     def save(self):
-        """ Commit the changes """
+        """commit changes to db"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ Remove an object """
+        """delete obj from db"""
         if obj:
             self.__session.delete(obj)
             self.save()
 
     def reload(self):
-        """ Initializates a session"""
+        """create all tables in the db"""
+        from models.base_model import Base
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.user import User
+        from models.review import Review
+        from models.place import Place
+        from sqlalchemy.orm import sessionmaker, scoped_session
+
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
         Base.metadata.create_all(self.__engine)
 
-        Session = sessionmaker(self.__engine, expire_on_commit=False)
-        ses = scoped_session(Session)
-        self.__session = ses()
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """Thread specific storage"""
+        self.__session.close()
